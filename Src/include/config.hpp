35394e948cb3fe35f1448f93fb81a10460602cab
Mr-Maks-S-A <salakkotov@gmail.com>
2025-01-06 22:08:49 +0300

    Minor project setup

--- Src/include/config.hpp
+++ Src/include/config.hpp
@@ -1,19 +1,33 @@
 #ifndef _CONFIG_GPP_
 #define _CONFIG_GPP_
 
-#define PLATFORM_ID ""
+#include <SDL3/SDL_init.h>
+
+// #define PLATFORM_ID ""
 
 #define PROGECT_LANG_CPP_STANDART (23)
-#define CXX_COMPILER_ID ""
-#define CXX_COMPILER_VERSION ()
+// #define CXX_COMPILER_ID ""
+// #define CXX_COMPILER_VERSION ()
 
 #define PROGECT_LANG_C_STANDART (17)
-#define C_COMPILER_ID ""
-#define C_COMPILER_VERSION ()
+// #define C_COMPILER_ID ""
+// #define C_COMPILER_VERSION ()
 
 #define PROGECT_VERSION_MAJOR (0)
 #define PROGECT_VERSION_MINOR (1)
 #define PROGECT_VERSION_PATCH (1)
 #define PROGECT_VERSION "0.1.1"
+
+static const struct {
+  const char* m_key;
+  const char* m_value;
+} extended_metadata[] = {{SDL_PROP_APP_METADATA_NAME_STRING, "3D_base"},
+                         {SDL_PROP_APP_METADATA_VERSION_STRING, PROGECT_VERSION},
+                         {SDL_PROP_APP_METADATA_IDENTIFIER_STRING, " SDL3 Render Project"},
+                         {SDL_PROP_APP_METADATA_CREATOR_STRING, "Jarco (ak Mr-Maks-S-A)"},
+                         {SDL_PROP_APP_METADATA_COPYRIGHT_STRING, "None"},
+                         {SDL_PROP_APP_METADATA_URL_STRING, "https://github.com/Mr-Maks-S-A/Training3D.git"},
+                         {SDL_PROP_APP_METADATA_TYPE_STRING, "Game"}};
+
 
 #endif //_CONFIG_GPP_
--- Src/main.cpp
+++ Src/main.cpp
@@ -1,11 +1,92 @@
-// #include <config.hpp>
-#include <iomanip>
+#define SDL_MAIN_USE_CALLBACKS /* use the callbacks instead of main() */
+#include <SDL3/SDL.h>
+#include <SDL3/SDL_main.h>
+
+#include <config.hpp>
 #include <iostream>
+#include <string_view>
+
+#define SDL_WINDOW_WIDTH 1024
+#define SDL_WINDOW_HEIGHT 700
+
+typedef struct {
+  SDL_Window *window;
+  SDL_Renderer *renderer;
+} AppState;
 
 
-auto main(int32_t agrc, char **agrv) -> int32_t {
+SDL_AppResult SDL_AppEvent(void *appstate, SDL_Event *event) {
+  switch (event->type) {
+    case SDL_EVENT_QUIT:
+      return SDL_APP_SUCCESS;
+    case SDL_EVENT_KEY_DOWN:
+      // return handle_key_event_(ctx, event->key.scancode);
+      break;
+  };
+  return SDL_APP_CONTINUE;
+}
 
-  std::cout<<"Hi!\n";
+SDL_AppResult SDL_AppIterate(void *appstate) {
+  AppState *as = (AppState *) appstate;
+  const Uint64 now = SDL_GetTicks();
+  SDL_FRect r;
+  r.h = 300;
+  r.w = 300;
+  r.x = 0;
+  r.y = 0;
 
-  return EXIT_SUCCESS;
-};
+
+  SDL_SetRenderDrawColor(as->renderer, 0, 255, 0, SDL_ALPHA_OPAQUE);
+  SDL_RenderClear(as->renderer);
+
+  SDL_SetRenderDrawColor(as->renderer, 255, 255, 0, SDL_ALPHA_OPAQUE); /*head*/
+  SDL_RenderFillRect(as->renderer, &r);
+  SDL_RenderPresent(as->renderer);
+  return SDL_APP_CONTINUE;
+}
+
+SDL_AppResult SDL_AppInit(void **appstate, int argc, char *argv[]) {
+  if (!SDL_SetAppMetadata(std::string(extended_metadata[0].m_value).c_str(),
+                          std::string(extended_metadata[1].m_value).c_str(),
+                          std::string(extended_metadata[2].m_value).c_str())) {
+    return SDL_APP_FAILURE;
+  }
+
+  for (size_t i = 0; i < SDL_arraysize(extended_metadata); ++i) {
+    if (!SDL_SetAppMetadataProperty(std::string(extended_metadata[i].m_key).c_str(),
+                                    std::string(extended_metadata[i].m_value).c_str())) {
+      return SDL_APP_FAILURE;
+    }
+  }
+
+  AppState *as = new AppState;
+  if (!as) {
+    return SDL_APP_FAILURE;
+  } else {
+    *appstate = as;
+  }
+
+  if (!SDL_Init(SDL_INIT_VIDEO)) {
+    return SDL_APP_FAILURE;
+  }
+
+  if (!SDL_CreateWindowAndRenderer(std::string(extended_metadata[2].m_value).c_str(), SDL_WINDOW_WIDTH,
+                                   SDL_WINDOW_HEIGHT, 0, &as->window, &as->renderer)) {
+    return SDL_APP_FAILURE;
+  }
+
+  SDL_SetRenderVSync(as->renderer, false);
+  // SDL_SetWindowResizable(as->window, SDL_WINDOW_RESIZABLE);
+  // SDL_SetWindowRelativeMouseMode(as->window, true);
+  // SDL_SetHintWithPriority(SDL_HINT_WINDOWS_RAW_KEYBOARD, "1", SDL_HINT_OVERRIDE);
+  return SDL_APP_CONTINUE;
+}
+
+void SDL_AppQuit(void *appstate, SDL_AppResult result) {
+  if (appstate != NULL) {
+    AppState *as = (AppState *) appstate;
+    SDL_DestroyRenderer(as->renderer);
+    SDL_DestroyWindow(as->window);
+    SDL_free(as);
+  }
+}
